22faea85a1d7415341f41a1716109aa74c670ddb
Vinnie <pablovasta1995@gmail.com>
2024-04-23 22:35:51 +0200

    instalamos dotenv, y conectamos con mongoose, comentamos las funciones.

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+
+const { error } = require("console")
+//Instalamos express para trabajar en el back
 const express = require("express")
+//Definimos nuestro puerto en Localhost
 const PORT = 4000
-
+//Instalamos mongoose para conectar a mongoDB
+const mongoose = require("mongoose")
+//Definimos constante app para levantar servidor con  express
 const app = express()
+//Instalamos dotenv para guardar claves como Tokens, Secret tokens y Url de mongodb con su contraseña
+require("dotenv").config()
+//Definimos la url de mongo llamando a .env en una constante
+const urlMongodb = process.env.MONGO_URL_PSWD
 
-app.get("/nombre", (req, res) =>{
-    res.setHeader("Content-type", "text/html; charset=utf-8")
-    res.end("<h2>Hola Pablo </h2>")
+//Conectaqmos con mongoose a la url que hemos definido antes
+mongoose.connect(urlMongodb)
+//Guardamos en una constante db la conexion.
+const db = mongoose.connection;
+
+//Y hacemos control de errores si se conecta o no
+//Si no se conecta, lanzamos error:
+db.on("error", (error) => {
+    console.error("Error al conectar")
 })
-app.get("/:parametro")
 
+//Si se conecta, lanzamos conexion establecida con db.once puesto que solo se va a ver una vez si la conexion esta hecha.
+db.once("connected", () => {
+    console.log("Connection success")
+})
+
+//Si se ha desconectado lanzamos otro db.on con otro error.
+db.on("disconnected", (error) => {
+    console.error("mongoose default conection is disconnected")
+})
+
+
+//Aqui lanzamos un app.listen para definir nuestra url para seguir el link desde la consola.
 app.listen(PORT, () => {
         console.log(`Server running in http://localhost:${PORT}`)
 })
+
